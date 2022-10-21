ef86847d689c3b0610cf978d3685ffca6ff0d244
Mitchell Wears <64507448+MitchellWears@users.noreply.github.com>
2022-10-21 11:48:33 -0400

    Add files via upload

--- include/parallel.h
+++ include/parallel.h
@@ -1,36 +1,61 @@
 #ifndef _UNICODE
+#define _UNICODE
 #endif
+
 #ifndef _WIN32
+#define _WIN32
 #endif
 
 #include <windows.h>
 
 typedef struct ParallelWindow ParallelWindow;
 
+typedef struct ParallelDevice ParallelDevice;
+
 struct ParallelWindow
 {
  HINSTANCE hInstance;
  HWND hwnd;
- const wchar_t* title;
- int width;
- int height;
+ const char* title;
+ int w;
+ int h;
+ int x;
+ int y;
  UINT flags;
- int shouldClose;
-
- ParallelWindow* next;
- ParallelWindow* prev;
- ParallelWindow* windowdata;
 };
 
-ParallelWindow* CreateParallelWindow(const wchar_t* title, int width, int height, UINT flags);
-void ParallelWindowWin32(ParallelWindow* window);
-void ParallelWindowEvents(ParallelWindow* window);
-void ParallelWindowWin32Events(ParallelWindow* window);
-void ParallelWindowWin32CloseRequest(ParallelWindow* window);
-void ParallelWindowCloseRequest(ParallelWindow* window);
-void ParallelWindowDestroyWindow(ParallelWindow* window);
-void ParallelWindowDestroyWindowWin32(ParallelWindow* window);
-void ParallelWindowQuitWin32(ParallelWindow* window);
-void ParallelWindowQuit(ParallelWindow* window);
-int ParallelWindowShouldClose(ParallelWindow* handle);
+union ParallelEvent
+{
+UINT32 PARALLEL_WINDOWEVENT;
+UINT32 PARALLEL_QUITEVENT;
+};
+
+struct ParallelDevice
+{
+    void (*parallelcreatewindow) (ParallelWindow* window);
+};
+
+/* Creates the Parallel window */
+ParallelWindow* CreateParallelWindow(const char* title, int x, int y, int w, int h, UINT flags);
+
+/* Win32 window */
+int ParallelWin32(ParallelWindow* window);
+
+/* Pump Events */
+void ParallelEvents(ParallelWindow* window);
+
+/* Win32 Events */
+void ParallelWin32Events(ParallelWindow* window);
+
+/* Get the window x y coordinates */
+void ParallelAdjustWin32Rect(ParallelWindow* window, DWORD style, int x, int y, int w, int h, UINT flags);
+
+/* Win32 Main */
+int Parallel_Main();
+
+/* ParallelWindow Initialize */
+void ParallelInit(UINT flags);
+
+
+
 
