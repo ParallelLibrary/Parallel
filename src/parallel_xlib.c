303b1e2191b8b6556bb54bcdadc703438b210c1f
MitchellWears <64507448+RetroGamingIsAwesome@users.noreply.github.com>
2022-12-13 10:19:04 -0500

    Update parallel_xlib.c

--- src/parallel_xlib.c
+++ src/parallel_xlib.c
@@ -5,6 +5,7 @@ int parallel_x11(ParallelWindow* window)
  Display* d;
  Window* w;
  int s;
+ s = XDefaultScreen(d);
  w = XCreateWindow(d, RootWindow(d, s), x, y, w, h, );
  d = XOpenDisplay(NULL);
 }
