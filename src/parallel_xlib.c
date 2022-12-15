e5b9bb798f1347383929139d6260bf199138f465
MitchellWears <64507448+RetroGamingIsAwesome@users.noreply.github.com>
2022-12-15 17:37:41 -0500

    Update parallel_xlib.c

--- src/parallel_xlib.c
+++ src/parallel_xlib.c
@@ -7,10 +7,20 @@ int parallel_x11(ParallelWindow* window)
  Window* w;
  XEvent e;
  XSetWindowAttributes a;
+ XTextProperty p;
+ p.value = (const char*)window->title;
+ p.encoding = XA_STRING;
+ p.format = 8;
+ p.nitems = strlen(title);
+
  int s;
 
  sc = DefaultScreenOfDisplay(d);
  s = DefaultScreen(d);
  w = XCreateWindow(d, RootWindow(d, s), x, y, w, h, 0, visual->depth, InputOutput, visual->visual, CWBackPixel | CWColorMap | CWCursor, &a);
  d = XOpenDisplay(NULL);
+
+ 
+
+ XSetWMName(d, w, &p);
 }
