a292c29b20c6910d86745c63d1e385c17bbe5076
MitchellWears <64507448+RetroGamingIsAwesome@users.noreply.github.com>
2023-01-01 10:16:55 -0500

    Update parallel_wgl.c

--- src/parallel_wgl.c
+++ src/parallel_wgl.c
@@ -8,5 +8,20 @@ ParallelWindow* window;
 GetHDC(window->hwnd);
 
 PIXELFORMATDESCRIPTOR pf = {
-
+sizeof(PIXELFORMATDESCRIPTOR),
+1,
+PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,
+PFD_TYPE_RGBA,
+32,
+0, 0, 0, 0, 0, 0,
+0,
+0,
+0,
+0, 0, 0, 0,
+24,
+8,
+0,
+PFD_MAIN_PLANE,
+0,
+0, 0, 0
 };
