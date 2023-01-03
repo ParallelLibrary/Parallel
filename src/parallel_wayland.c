0a2f144299247983bd6fcaf72b23717dedd3fa1a
MitchellWears <64507448+RetroGamingIsAwesome@users.noreply.github.com>
2023-01-03 12:12:40 -0500

    Update parallel_wayland.c

--- src/parallel_wayland.c
+++ src/parallel_wayland.c
@@ -6,10 +6,14 @@ wl_display* d;
 wl_event_loop* e;
 wl_compositor* c;
 wl_region* r;
+wl_buffer* b;
 wl_surface* s;
 
 d = wl_display_connect(NULL);
 d = wl_display_create(NULL);
 
-r = wl_display_create_region(c);
+if (window->width || window->height)
+{
+}
+
 }
