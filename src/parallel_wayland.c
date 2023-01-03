d06bb715ae9aa8feec8c842be0622c2c8b3f3a84
MitchellWears <64507448+RetroGamingIsAwesome@users.noreply.github.com>
2023-01-03 07:33:16 -0500

    Update parallel_wayland.c

--- src/parallel_wayland.c
+++ src/parallel_wayland.c
@@ -5,9 +5,11 @@ int parallelwayland(ParallelWindow* window)
 wl_display* d;
 wl_event_loop* e;
 wl_compositor* c;
+wl_region* r;
 wl_surface* s;
 
 d = wl_display_connect(NULL);
 d = wl_display_create(NULL);
 
+r = wl_display_create_region(c);
 }
