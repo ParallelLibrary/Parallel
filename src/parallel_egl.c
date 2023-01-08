53bf5f48c255a7ab86d5266642767c1eb73e20e4
MitchellWears <64507448+RetroGamingIsAwesome@users.noreply.github.com>
2023-01-08 18:14:49 -0500

    Update parallel_egl.c

--- src/parallel_egl.c
+++ src/parallel_egl.c
@@ -9,6 +9,8 @@ EGLContext* con;
 
 wl_egl_window* w;
 
+int major, minor, count, n, size;
+
 w = wl_egl_window_create(s, window->w, window->h);
 s = egl_create_surface(d, c, w, NULL);
 
@@ -28,5 +30,17 @@ EGL_CONTEXT_CLIENT_VERSION, 3,
 EGL_NONE
 }
 
+d = eglGetDisplay(window->display);
 
+eglInitialize(d, &major, &minor);
+
+eglGetConfigs(d, NULL, 0, &count);
+
+eglChooseConfig(d, glAttribs, c, count, &n);
+
+eglGetConfigAttribs(d, c, EGL_BUFFER_SIZE, &size);
+
+con = eglCreateContext(d, c, EGL_NO_CONTEXT, &contextAttribs);
+
+eglMakeCurrent(d, s, 0, c);
 }
