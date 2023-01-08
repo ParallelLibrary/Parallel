10a79d87ed3e2508fef18bcf071c58c88b556c18
MitchellWears <64507448+RetroGamingIsAwesome@users.noreply.github.com>
2023-01-08 16:45:54 -0500

    Update parallel_egl.c

--- src/parallel_egl.c
+++ src/parallel_egl.c
@@ -9,7 +9,10 @@ EGLContext* con;
 
 wl_egl_window* w;
 
+w = wl_egl_window_create(s, window->w, window->h);
+s = egl_create_surface(d, c, w, NULL);
 
+eglMakeCurrent(d, s, 0, c);
 
 int glAtttibs[] = {
 EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
