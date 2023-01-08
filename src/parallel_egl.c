246a3138ffae7104b2efaa2ccca3678e806c6a83
MitchellWears <64507448+RetroGamingIsAwesome@users.noreply.github.com>
2023-01-08 16:02:25 -0500

    Update parallel_egl.c

--- src/parallel_egl.c
+++ src/parallel_egl.c
@@ -2,5 +2,18 @@
 
 int parallelegl(ParallelWindow* window)
 {
+int glAtttibs[] = {
+EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
+EGL_RED_SIZE, 8,
+EGL_GREEN_SIZE, 8,
+EGL_BLUE_SIZE, 8,
+EGL_RENDERABLE_TYPE, EGL_OPENGL_ES3_BIT,
+EGL_NONE
+}
+
+int contextAttribs[] = {
+EGL_CONTEXT_CLIENT_VERSION, 3,
+EGL_NONE
+}
 
 }
