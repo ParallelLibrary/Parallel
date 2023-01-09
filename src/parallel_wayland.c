e99114933601cff4109a2ca291b1464a7bd2fb8a
MitchellWears <64507448+RetroGamingIsAwesome@users.noreply.github.com>
2023-01-09 14:16:01 -0500

    Update parallel_wayland.c

--- src/parallel_wayland.c
+++ src/parallel_wayland.c
@@ -2,7 +2,6 @@
 
 int parallelwayland(ParallelWindow* window)
 {
-wl_display* d;
 wl_event_loop* e;
 wl_compositor* c;
 wl_region* r;
@@ -12,6 +11,7 @@ wl_surface* s;
 wl_listener* l;
 wl_registry* reg;
 wl_keyboard* k;
+wl_pointer* p;
 const wl_keyboard_listener* kl;
 const wl_interface* i;
 
@@ -21,7 +21,6 @@ d = wl_display_create(NULL);
 if (window->width || window->height)
 {
 }
-
 s = wl_compositor_create_surface(c);
 
 wl_surface_attach(&s, &b, window->width, window->height);
@@ -32,13 +31,13 @@ wl_buffer_add_listener(&b, &bl, NULL);
 
 wl_keyboard_add_listener(&k, &kl, NULL);
 
-wl_keyboard_destroy(&d);
+wl_keyboard_destroy(&k);
 
 wl_pointer_set_cursor(&p, NULL, &s, 0, 0);
 
-wl_pointer_destroy(&d);
+wl_pointer_destroy(&p);
 
-wl_get_registery(display);
+wl_get_registery(window->d);
 
 wl_registry_add_listener(wl_registry, &l, NULL);
 
